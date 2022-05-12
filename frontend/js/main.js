2d1be9e88b54a1b1fda4d0c3bcbfd70a8ac73524
moekidev <me@moeki.dev>
2022-05-12 21:34:16 +0900

    rm js

--- frontend/js/main.js
+++
@@ -1,5 +0,0 @@
-import { Application } from '@hotwired/stimulus'
-import { RatingController } from './controllers/rating-controller'
-
-const app = Application.start()
-app.register('rating', RatingController)
